da985633d631d576c1dd701b2846536b45c70bf0
Kurrgat <mrcykj@gmail.com>
2023-10-18 19:46:53 +0300

    Updates to the simple_shell project

--- shell.h
+++ shell.h
@@ -18,8 +18,6 @@ typedef struct CommandInfo
 	int err_num;
 } info_t;
 
-// Add other necessary typedefs
-
 /* Function prototypes */
 int _myexit(info_t *info);
 int _mycd(info_t *info);
@@ -29,12 +27,19 @@ int _puts_norm(char *s);
 void _puts(char *s);
 int _putchar(char c);
 int _strlen(const char *s);
-
-/* Prototypes for alias_functions.c functions */
+int print_environment(info_t *info);
+char *get_environment_variable(info_t *info, const char *name);
+int set_environment_variable(info_t *info);
+int unset_environment_variable(info_t *info);
+int populate_environment_list(info_t *info);
 int _myhistory(info_t *info);
 int unset_alias(info_t *info, char *str);
 int set_alias(info_t *info, char *str);
 int print_alias(list_t *node);
 int _myalias(info_t *info);
+void _eputs(char *str);
+int _eputchar(char c);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
 
 #endif /* SHELL_H */
